3b83e24371824c8a78942dbffdde269411e9850d
RekaRajendran <reka1901@gmail.com>
2025-10-16 12:47:47 +0530

    Requesthandler

--- src/app.js
+++ src/app.js
@@ -2,15 +2,22 @@ const express = require('express');
 
 const app=express();
 //Request handler
-app.use("/",(req,res)=>{
-    res.send("Hello from the dashboard")
-})
-app.use("/hello",(req,res)=>{
-    res.send("hello hello hello")
-})
+//This will only handle GET call to user
+app.get("/user",(req,res)=>{
+    res.send({firstName:"Reka",lastName:"Mari"})
+});
+app.post("/user",(req,res)=>{
+    res.send("Data successfully saved to the database")
+});
+app.delete("/user",(req,res)=>{
+    res.send("Detled successfully ")
+});
+//This will match all the HTTP method API calls to /test
 app.use("/test",(req,res)=>{
-    res.send("Hello from the server")
+    res.send("Hello from the server - test")
 })
+
+
 app.listen(3000,()=>{
-    console.log("server is successfully listening on port 300")
+    console.log("server is successfully listening on port  3000")
 });
